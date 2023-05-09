40019332e8b2c981498b3027cc888eb61a86b093
neto test <seliwanow08@gmail.com>
2023-05-09 12:27:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 14365;
+        int percent = 5;
+        int bonus = amount * percent / 100;
+        System.out.println("Бонусные мили=" + bonus);
+
+    }
+}
